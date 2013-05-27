9c6264e7f33eba1b4fe74457469fe94971f065a6
chineking <chinekingseu@gmail.com>
2013-05-27 14:47:11 +0800

    add match to url patterns

--- cola/core/urls.py
+++ cola/core/urls.py
@@ -6,21 +6,31 @@ Created on 2013-5-21
 @author: Chine
 '''
 
-class Url(object):
+class UrlPattern(object):
     def __init__(self, url_re, name, parser):
         self.url_re = url_re
         self.name = name
         self.parser = parser
         
+    def match(self, url):
+        return self.url_re.match(url) is not None
+        
 class UrlPatterns(object):
     def __init__(self, *urls):
         for url in urls:
-            if not isinstance(url, Url):
+            if not isinstance(url, UrlPattern):
                 raise ValueError('urls must be Url instances')
         self.url_patterns = list(urls)
         
     def __add__(self, url_obj):
-        if not isinstance(url_obj, Url):
+        if not isinstance(url_obj, UrlPattern):
             raise ValueError('url_obj must be an instance of Url')
         self.url_patterns.append(url_obj)
         return self
+    
+    def matches(self, urls):
+        for url in urls:
+            for pattern in self.url_patterns:
+                if pattern.match(url):
+                    yield url
+                    break

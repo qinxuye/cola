4fd8421510fe94223c465dbbc7a46742b763a2cf
Chine King <chinekingseu@gmail.com>
2013-08-13 01:32:51 +0800

    revert some operations of last commit

--- contrib/weibo/bundle.py
+++ contrib/weibo/bundle.py
@@ -42,8 +42,8 @@ class WeiboUserBundle(Bundle):
         start = int(time.time() * (10**6))
         return [
             'http://weibo.com/%s/follow' % self.uid,
-#             'http://weibo.com/aj/mblog/mbloglist?uid=%s&_k=%s' % (self.uid, start),
-#             'http://weibo.com/%s/info' % self.uid,
+            'http://weibo.com/aj/mblog/mbloglist?uid=%s&_k=%s' % (self.uid, start),
+            'http://weibo.com/%s/info' % self.uid,
             # remove because some user's link has been http://weibo.com/uid/follow?relate=fans
             # 'http://weibo.com/%s/fans' % self.uid
         ]

b20107d1a23c989cd8549819e8486d3696cb360f
chineking <chinekingseu@gmail.com>
2014-03-30 16:36:01 +0800

    modify the weibo login caused by the change by weibo

--- contrib/weibo/login.py
+++ contrib/weibo/login.py
@@ -95,6 +95,11 @@ class WeiboLogin(object):
             }
             postdata = urllib.urlencode(postdata)
             text = self.opener.open(login_url, postdata)
+
+            # Fix for new login changed since about 2014-3-28
+            ajax_url_regex = re.compile('location\.replace\(\'(.*)\'\)')
+            ajax_url = ajax_url_regex.search(text).group(1)
+            text = self.opener.open(ajax_url)
             
             regex = re.compile('\((.*)\)')
             json_data = json.loads(regex.search(text).group(1))

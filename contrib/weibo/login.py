9132cfc64955d327a4d703aa597c8e0d96c82393
chineking <chinekingseu@gmail.com>
2014-03-31 22:58:49 +0800

    some accounts are not effected by the new login mechanism, fix the issue

--- contrib/weibo/login.py
+++ contrib/weibo/login.py
@@ -98,8 +98,10 @@ class WeiboLogin(object):
 
             # Fix for new login changed since about 2014-3-28
             ajax_url_regex = re.compile('location\.replace\(\'(.*)\'\)')
-            ajax_url = ajax_url_regex.search(text).group(1)
-            text = self.opener.open(ajax_url)
+            matches = ajax_url_regex.search(text)
+            if matches is not None:
+                ajax_url = matches.group(1)
+                text = self.opener.open(ajax_url)
             
             regex = re.compile('\((.*)\)')
             json_data = json.loads(regex.search(text).group(1))

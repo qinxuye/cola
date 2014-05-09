3b1adbbdae00ef7b0f10771b2f5c9fbfef460c14
chineking <chinekingseu@gmail.com>
2014-05-10 00:27:31 +0800

    fix commented code

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -47,70 +47,70 @@ class Test(unittest.TestCase):
         finally:
             shutil.rmtree(self.dir_)
 
-#     def testBudgetApply(self):
-#         self.serv = BudgetApplyServer(self.dir_, Settings(), rpc_server=self.rpc_server)
-#         self.cli1 = BudgetApplyClient(self.serv)
-#         self.cli2 = BudgetApplyClient('localhost:%s'%self.port)
-#            
-#         try:
-#             self.serv.set_budgets(90)
-#             self.assertEqual(self.cli1.apply(50), 50)
-#             self.assertEqual(self.cli2.apply(50), 40)
-#                
-#             self.cli1.finish(50)
-#             self.assertEqual(50, self.serv.finished)
-#             self.cli2.finish(50)
-#             self.assertEqual(90, self.serv.finished)
-#                
-#             self.cli1.error(10)
-#             self.assertEqual(90, self.serv.applied)
-#             self.serv.finished = 0
-#             self.cli2.error(10)
-#             self.assertEqual(80, self.serv.applied)
-#         finally:
-#             self.serv.shutdown()
-#              
-#     def testCounter(self):
-#         self.serv = CounterServer(self.dir_, Settings(),
-#                                   rpc_server=self.rpc_server)
-#         self.cli1 = CounterClient(self.serv)
-#         self.cli2 = CounterClient('localhost:%s'%self.port)
-#          
-#         try:
-#             self.cli1.global_inc('pages', 10)
-#             self.cli2.global_inc('pages', 2)
-#             self.assertEqual(self.cli1.get_global_inc('pages'), 10)
-#             self.assertEqual(self.cli2.get_global_inc('pages'), 2)
-#             self.assertEqual(self.serv.inc_counter.get('global', 'pages', 0), 0)
-#              
-#             self.cli1.sync()
-#             self.cli2.sync()
-#             self.assertEqual(self.cli1.get_global_inc('pages'), None)
-#             self.assertEqual(self.cli2.get_global_inc('pages'), None)
-#             self.assertEqual(self.serv.inc_counter.get('global', 'pages'), 12)
-#              
-#             self.cli1.local_inc(self.addr, 0, 'pages', 100)
-#             self.assertEqual(self.cli1.get_local_inc(self.addr, 0, 'pages'), 100)
-#             self.cli1.sync()
-#             self.assertEqual(self.serv.inc_counter.get('%s#%s'%(self.addr, 0), 'pages'), 100)
-#              
-#             self.cli1.global_acc('normal', 100)
-#             self.cli2.global_acc('normal', 'test')
-#             self.assertEqual(self.cli1.get_global_acc('normal'), [100,])
-#             self.assertEqual(self.cli2.get_global_acc('normal'), ['test',])
-#              
-#             self.cli1.sync()
-#             self.cli2.sync()
-#             self.assertEqual(self.cli1.get_global_acc('normal'), None)
-#             self.assertEqual(self.cli2.get_global_acc('normal'), None)
-#             self.assertEqual(self.serv.acc_counter.get('global', 'normal'), [100, 'test'])
-#              
-#             self.cli2.local_acc(self.addr, 1, 'normal', 100)
-#             self.assertEqual(self.cli2.get_local_acc(self.addr, 1, 'normal'), [100, ])
-#             self.cli2.sync()
-#             self.assertEqual(self.serv.acc_counter.get('%s#%s'%(self.addr, 1), 'normal'), [100, ])
-#         finally:
-#             self.serv.shutdown()
+    def testBudgetApply(self):
+        self.serv = BudgetApplyServer(self.dir_, Settings(), rpc_server=self.rpc_server)
+        self.cli1 = BudgetApplyClient(self.serv)
+        self.cli2 = BudgetApplyClient('localhost:%s'%self.port)
+            
+        try:
+            self.serv.set_budgets(90)
+            self.assertEqual(self.cli1.apply(50), 50)
+            self.assertEqual(self.cli2.apply(50), 40)
+                
+            self.cli1.finish(50)
+            self.assertEqual(50, self.serv.finished)
+            self.cli2.finish(50)
+            self.assertEqual(90, self.serv.finished)
+                
+            self.cli1.error(10)
+            self.assertEqual(90, self.serv.applied)
+            self.serv.finished = 0
+            self.cli2.error(10)
+            self.assertEqual(80, self.serv.applied)
+        finally:
+            self.serv.shutdown()
+              
+    def testCounter(self):
+        self.serv = CounterServer(self.dir_, Settings(),
+                                  rpc_server=self.rpc_server)
+        self.cli1 = CounterClient(self.serv)
+        self.cli2 = CounterClient('localhost:%s'%self.port)
+          
+        try:
+            self.cli1.global_inc('pages', 10)
+            self.cli2.global_inc('pages', 2)
+            self.assertEqual(self.cli1.get_global_inc('pages'), 10)
+            self.assertEqual(self.cli2.get_global_inc('pages'), 2)
+            self.assertEqual(self.serv.inc_counter.get('global', 'pages', 0), 0)
+              
+            self.cli1.sync()
+            self.cli2.sync()
+            self.assertEqual(self.cli1.get_global_inc('pages'), None)
+            self.assertEqual(self.cli2.get_global_inc('pages'), None)
+            self.assertEqual(self.serv.inc_counter.get('global', 'pages'), 12)
+              
+            self.cli1.local_inc(self.addr, 0, 'pages', 100)
+            self.assertEqual(self.cli1.get_local_inc(self.addr, 0, 'pages'), 100)
+            self.cli1.sync()
+            self.assertEqual(self.serv.inc_counter.get('%s#%s'%(self.addr, 0), 'pages'), 100)
+              
+            self.cli1.global_acc('normal', 100)
+            self.cli2.global_acc('normal', 'test')
+            self.assertEqual(self.cli1.get_global_acc('normal'), [100,])
+            self.assertEqual(self.cli2.get_global_acc('normal'), ['test',])
+              
+            self.cli1.sync()
+            self.cli2.sync()
+            self.assertEqual(self.cli1.get_global_acc('normal'), None)
+            self.assertEqual(self.cli2.get_global_acc('normal'), None)
+            self.assertEqual(self.serv.acc_counter.get('global', 'normal'), [100, 'test'])
+              
+            self.cli2.local_acc(self.addr, 1, 'normal', 100)
+            self.assertEqual(self.cli2.get_local_acc(self.addr, 1, 'normal'), [100, ])
+            self.cli2.sync()
+            self.assertEqual(self.serv.acc_counter.get('%s#%s'%(self.addr, 1), 'normal'), [100, ])
+        finally:
+            self.serv.shutdown()
             
     def testSpeedControl(self):
         settings = Settings()
